eeddf2c9964f53ab6c87b96362488f879586e4eb
Wojciech Weremczuk <wojciech.weremczuk5@gmail.com>
2023-01-15 15:28:11 +0100

    Added Spring backend module

---
+++ backend/src/main/java/mimuw/backend/entity/Event.java
@@ -0,0 +1,33 @@
+package mimuw.backend.entity;
+
+//import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+//@Entity
+//@Table(name = "event")
+public class Event {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+//    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+//    @Column(nullable = false)
+    private String beginDate;
+
+//    @Column(nullable = false)
+    private String endDate;
+
+//    @Column(nullable = false)
+    private Integer archived;
+}
