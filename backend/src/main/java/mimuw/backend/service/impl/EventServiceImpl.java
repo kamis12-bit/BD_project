4b682b15e8357c0e89c184c1b3d6c8184bbe58da
Wojciech Weremczuk <ww438808@students.mimuw.edu.pl>
2023-01-16 16:08:34 +0100

    Connected to Oracle Database

--- backend/src/main/java/mimuw/backend/service/impl/EventServiceImpl.java
+++ backend/src/main/java/mimuw/backend/service/impl/EventServiceImpl.java
@@ -2,7 +2,7 @@ package mimuw.backend.service.impl;
 
 import lombok.AllArgsConstructor;
 import mimuw.backend.entity.Event;
-//import mimuw.backend.repository.EventRepository;
+import mimuw.backend.repository.EventRepository;
 import mimuw.backend.service.EventService;
 import org.springframework.stereotype.Service;
 
@@ -11,40 +11,40 @@ import java.util.List;
 @Service
 @AllArgsConstructor
 public class EventServiceImpl implements EventService {
-//    private EventRepository eventRepository;
+    private EventRepository eventRepository;
 
     @Override
     public Event createEvent(Event event) {
-//        return eventRepository.save(event);
-        return null;
+        return eventRepository.save(event);
+//        return null;
     }
 
     @Override
     public Event updateEvent(Event event) {
-//        Event eventToUpdate = eventRepository.findById(event.getId()).orElseThrow();
-//        eventToUpdate.setName(event.getName());
-//        eventToUpdate.setDescription(event.getDescription());
-//        eventToUpdate.setBeginDate(event.getBeginDate());
-//        eventToUpdate.setEndDate(event.getEndDate());
-//        eventToUpdate.setArchived(event.getArchived());
-//        return eventRepository.save(eventToUpdate);
-        return null;
+        Event eventToUpdate = eventRepository.findById(event.getId()).orElseThrow();
+        eventToUpdate.setName(event.getName());
+        eventToUpdate.setDescription(event.getDescription());
+        eventToUpdate.setBeginDate(event.getBeginDate());
+        eventToUpdate.setEndDate(event.getEndDate());
+        eventToUpdate.setArchived(event.getArchived());
+        return eventRepository.save(eventToUpdate);
+//        return null;
     }
 
     @Override
     public void deleteEvent(Long id) {
-//        eventRepository.deleteById(id);
+        eventRepository.deleteById(id);
     }
 
     @Override
     public Event getEventById(Long id) {
-//        return eventRepository.findById(id).orElseThrow();
-        return null;
+        return eventRepository.findById(id).orElseThrow();
+//        return null;
     }
 
     @Override
     public List<Event> getAllEvents() {
-//        return eventRepository.findAll();
-        return null;
+        return eventRepository.findAll();
+//        return null;
     }
 }
