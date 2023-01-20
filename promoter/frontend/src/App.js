92bb4dc685df60e1ea9e34751dbb6adc8a789bd3
Wojciech Weremczuk <ww438808@students.mimuw.edu.pl>
2023-01-20 10:59:02 +0100

    Small changes

--- promoter/frontend/src/App.js
+++ promoter/frontend/src/App.js
@@ -1,28 +1,25 @@
-import React, {Component, useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App () {
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    fetch('/api/event/all')
-        .then(response => response.text())
-        .then(message => {
-          setMessage(message);
-        });
-  },[])
+function App() {
   return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <h1 className="App-title">{message}</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
-  )
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
